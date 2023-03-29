b269db8c9f4811e52d52b3b195e688354e18ec24
Vatsook1996 <ahmetdianovv@mail.ru>
2023-03-29 21:04:04 +0300

    fixex88

--- src/test/java/com/demoga/TestBOX.java
+++ src/test/java/com/demoga/TestBOX.java
@@ -12,56 +12,44 @@ import static com.codeborne.selenide.Selenide.*;
 public class TestBOX extends TestBase {
     @Test
     void fullTestBOX() {
-        String firstName = "Rinat",
-                lastName = "Akhmetdiyanov",
-                email = "ahmetdianov@mail.ru",
-                gender = "Male",
-                phoneNumber = "9164077263",
-                birthMonth = "April",
-                birthYear = "1996",
-                birthDay = "21",
-                hobbies = "Sports",
-                picturePath = "images/image.png",
-                pictureName = "image.png",
-                state = "NCR",
-                city = "Noida";
+
         open("/automation-practice-form");
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
 
-        $("#firstName").setValue("Lyffi");
-        $("#lastName").setValue("Mugivara");
-        $("#userEmail").setValue("Lyffi@gmail.com");
+        $("#firstName").setValue("Ivanov");
+        $("#lastName").setValue("Ivan");
+        $("#userEmail").setValue("Ivan@mail.com");
         $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("9999999999");
+        $("#userNumber").setValue("9000000000");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").$(byText("July")).click();
-        $(".react-datepicker__year-select").$(byText("1987")).click();
-        $(".react-datepicker__day--0" + "12").click();
-        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $(".react-datepicker__month-select").$(byText("April")).click();
+        $(".react-datepicker__year-select").$(byText("1996")).click();
+        $(".react-datepicker__day--0" + "2").click();
+        $("#subjectsInput").setValue("English").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/cat.png"));
-        $("#currentAddress").setValue("Grande Line");
+        $("#uploadPicture").uploadFile(new File("src/test/resources/P-75.png"));
+        $("#currentAddress").setValue("Moscow");
         $("#state").click();
-        $(byText("Haryana")).click();
+        $(byText("Rajasthan")).click();
         $("#city").click();
-        $(byText("Karnal")).click();
+        $(byText("Jaiselmer")).click();
 
         $("#submit").click();
         $(".modal-dialog").should(appear);
         $(".table-responsive").shouldHave(
-                text("Lyffi"),
-                text("Mugivara"),
-                text("Lyffi@gmail.com"),
+                text("Ivanov),
+                text("Ivan"),
+                text("Ivan@mail.com"),
                 text("Male"),
-                text("9999999999"),
-                text("12 July,1987"),
-                text("Computer Science"),
+                text("9000000000"),
+                text("21 April,1996"),
+                text("English"),
                 text("Sports"),
-                text("cat.png"),
-                text("Grande Line"),
-                text("Haryana"),
-                text("Karnal")
+                text("P-75.png"),
+                text("Moscow"),
+                text("Rajasthan"),
+                text("Jaiselmer")
         );
     }
 }
